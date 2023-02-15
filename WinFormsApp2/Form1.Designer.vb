f2ce3062d902004b55afb0b7e3b0ef75f9bb27ed
NaufalJulio <Naufaljulio34@gmail.com>
2023-02-15 20:26:19 +0700

    first commit

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.load = New System.Windows.Forms.Button()
+        Me.save = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.List3 = New System.Windows.Forms.ListBox()
+        Me.List2 = New System.Windows.Forms.ListBox()
+        Me.List = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.clrBtn = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(115, 170)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(9, 170)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 15)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Awal"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(151, 152)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Akhir"
+        '
+        'load
+        '
+        Me.load.Location = New System.Drawing.Point(9, 12)
+        Me.load.Name = "load"
+        Me.load.Size = New System.Drawing.Size(101, 23)
+        Me.load.TabIndex = 12
+        Me.load.Text = "Load Config"
+        Me.load.UseVisualStyleBackColor = True
+        '
+        'save
+        '
+        Me.save.Location = New System.Drawing.Point(116, 12)
+        Me.save.Name = "save"
+        Me.save.Size = New System.Drawing.Size(87, 23)
+        Me.save.TabIndex = 13
+        Me.save.Text = "Save Config"
+        Me.save.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(255, 206)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Ganjil"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(145, 206)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Genap"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 206)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 15)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Normal"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(246, 170)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Proses"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'List3
+        '
+        Me.List3.FormattingEnabled = True
+        Me.List3.ItemHeight = 15
+        Me.List3.Location = New System.Drawing.Point(223, 224)
+        Me.List3.Name = "List3"
+        Me.List3.Size = New System.Drawing.Size(100, 274)
+        Me.List3.TabIndex = 5
+        '
+        'List2
+        '
+        Me.List2.FormattingEnabled = True
+        Me.List2.ItemHeight = 15
+        Me.List2.Location = New System.Drawing.Point(117, 224)
+        Me.List2.Name = "List2"
+        Me.List2.Size = New System.Drawing.Size(100, 274)
+        Me.List2.TabIndex = 4
+        '
+        'List
+        '
+        Me.List.FormattingEnabled = True
+        Me.List.ItemHeight = 15
+        Me.List.Location = New System.Drawing.Point(11, 224)
+        Me.List.Name = "List"
+        Me.List.Size = New System.Drawing.Size(100, 274)
+        Me.List.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Setting"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(17, 52)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(60, 19)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Genap"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(17, 75)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(56, 19)
+        Me.CheckBox3.TabIndex = 1
+        Me.CheckBox3.Text = "Ganjil"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(17, 26)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(66, 19)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Normal"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'clrBtn
+        '
+        Me.clrBtn.Location = New System.Drawing.Point(246, 12)
+        Me.clrBtn.Name = "clrBtn"
+        Me.clrBtn.Size = New System.Drawing.Size(75, 23)
+        Me.clrBtn.TabIndex = 15
+        Me.clrBtn.Text = "Clear"
+        Me.clrBtn.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 502)
+        Me.Controls.Add(Me.clrBtn)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.save)
+        Me.Controls.Add(Me.load)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.List3)
+        Me.Controls.Add(Me.List2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.List)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents load As Button
+    Friend WithEvents save As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents List3 As ListBox
+    Friend WithEvents List2 As ListBox
+    Friend WithEvents List As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents clrBtn As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Dim awal, akhir, i As Integer
+
+    Private Sub save_Click(sender As Object, e As EventArgs) Handles save.Click
+        Dim checkBox1State As Boolean = CheckBox1.Checked
+        Dim checkBox2State As Boolean = CheckBox2.Checked
+        Dim checkBox3State As Boolean = CheckBox3.Checked
+
+        Dim fileName As String = "checkboxes.txt"
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(fileName, False)
+
+        file.WriteLine(checkBox1State)
+        file.WriteLine(checkBox2State)
+        file.WriteLine(checkBox3State)
+
+        file.Close()
+        MessageBox.Show("Pengaturan kotak centang berhasil disimpan ke dalam file txt.")
+    End Sub
+
+    Private Sub load_Click(sender As Object, e As EventArgs) Handles load.Click
+        Dim fileName As String = "checkboxes.txt"
+        Dim file As System.IO.StreamReader
+        file = My.Computer.FileSystem.OpenTextFileReader(fileName)
+
+        Dim checkBox1State As Boolean = Boolean.Parse(file.ReadLine())
+        Dim checkBox2State As Boolean = Boolean.Parse(file.ReadLine())
+        Dim checkBox3State As Boolean = Boolean.Parse(file.ReadLine())
+
+        CheckBox1.Checked = checkBox1State
+        CheckBox2.Checked = checkBox2State
+        CheckBox3.Checked = checkBox3State
+
+        file.Close()
+
+        MessageBox.Show("Pengaturan kotak centang berhasil dimuat dari file txt.")
+    End Sub
+
+    Private Sub clrBtn_Click(sender As Object, e As EventArgs) Handles clrBtn.Click
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        List.Items.Clear()
+        List2.Items.Clear()
+        List3.Items.Clear()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        List.Items.Clear()
+        List2.Items.Clear()
+        List3.Items.Clear()
+        If CheckBox1.Checked = False And CheckBox2.Checked = False And CheckBox3.Checked = False Then
+            MessageBox.Show("Silahkan pilih opsi terlebih dahulu")
+        End If
+        If CheckBox1.Checked = True Then
+            akhir = TextBox1.Text
+            awal = TextBox2.Text
+            For i = awal To akhir
+                List.Items.Add(i)
+            Next
+        End If
+
+        If CheckBox2.Checked = True Then
+            akhir = TextBox1.Text
+            awal = TextBox2.Text
+            For i = awal To akhir
+                If i Mod 2 = 0 Then
+                    List2.Items.Add(i)
+                End If
+            Next
+        End If
+
+        If CheckBox3.Checked = True Then
+            akhir = TextBox1.Text
+            awal = TextBox2.Text
+            For i = awal To akhir
+                If i Mod 2 <> 0 Then
+                    List3.Items.Add(i)
+                End If
+            Next
+        End If
+    End Sub
+End Class
