02ecac47cc3e136d3174989f743ed98883a57dcc
NaufalJulio <125469194+NaufalJulio@users.noreply.github.com>
2025-02-14 21:58:25 +0700

    Add files via upload

---
+++ fsf-data.lua
@@ -0,0 +1,162 @@
+local data_version = "2.1"
+local data_versid = "r0yiIGZ1KlieTNey"
+local updmsg = "Features in GUI"
+local settingchanged = true
+local settingmsg = "Changed name system so many settings are wiped!"
+local link = "https://discord.gg/fWncS2vFx"
+
+local sunkenchestcoords = {
+-- Moosewood
+    {x = 936, y = 130, z = -159},
+    {x = 693, y = 130, z = -362},
+    {x = 613, y = 130, z = 498},
+    {x = 285, y = 130, z = 564},
+    {x = 283, y = 130, z = -159},
+-- Roslit Bay
+    {x = -1179, y = 130, z = 565},
+    {x = -1217, y = 130, z = 201},
+    {x = -1967, y = 130, z = 980},
+    {x = -2444, y = 130, z = 266},
+    {x = -2444, y = 130, z = -37},
+-- Sunstone Island
+    {x = -852, y = 130, z = -1560},
+    {x = -1000, y = 130, z = -751},
+    {x = -1500, y = 130, z = -750},
+    {x = -1547, y = 130, z = -1080},
+    {x = -1618, y = 130, z = -1560},
+-- Terrapin Island
+    {x = 798, y = 130, z = 1667},
+    {x = 562, y = 130, z = 2455},
+    {x = 393, y = 130, z = 2435},
+    {x = -1, y = 130, z = 1632},
+    {x = -190, y = 130, z = 2450},
+-- Mushgrove Swamp
+    {x = 2890, y = 130, z = -997},
+    {x = 2729, y = 130, z = -1098},
+    {x = 2410, y = 130, z = -1110},
+    {x = 2266, y = 130, z = -721},
+-- Forsaken Shores
+    {x = -2460, y = 130, z = 2047},
+} -- coords sourced from Fisch Wiki
+
+local defaultConfig = {
+    autoscan = true,
+    autohop = false,
+
+    autowebhook = false,
+    webhookUrl = "https://discord.com/api/webhooks/#/#",
+
+    filename = "servers",
+
+    infoList = {
+        autouptime = false,
+        autoversion = false,
+        autoplaceversion = false,
+    },
+
+    weatherList = {
+        ["Clear"] = false,
+        ["Foggy"] = false,
+        ["Windy"] = false,
+        ["Rain"] = false,
+        ["Eclipse"] = false,
+        ["Aurora Borealis"] = false,
+    },
+
+    eventList = {
+        ["Night of the Fireflies"] = false,
+        ["Night of the Luminous"] = false,
+        ["Shiny Surge"] = false,
+        ["Mutation Surge"] = false,
+    },
+
+    seasonList = {
+        ["Spring"] = false,
+        ["Summer"] = false,
+        ["Fall"] = false,
+        ["Winter"] = false,
+    },
+
+    cycleList = {
+        ["Day"] = false,
+        ["Night"] = false,
+    },
+
+    luckList = {
+        enabled = false,
+        min = 8,
+    },
+
+    versionList = {
+        enabled = false,
+        version = "x.x",
+    },
+
+    placeVersionList = {
+        enabled = false,
+        version = 1234,
+    },
+    
+    uptimeList = {
+        beforeTime = {
+            enabled = false,
+            hour = 0,
+            minute = 0,
+        },
+
+        afterTime = {
+            enabled = false,
+            hour = 0,
+            minute = 0,
+        },
+
+        orLogic = true,
+    },
+
+    zoneList = {
+        ["Megalodon Default"] = false,
+        ["Megalodon Ancient"] = false,
+        ["Great White Shark"] = false,
+        ["Great Hammerhead Shark"] = false,
+        ["Whale Shark"] = false,
+        ["The Kraken Pool"] = false,
+        ["Ancient Kraken Pool"] = false,
+        ["Orcas Pool"] = false,
+        ["Ancient Orcas Pool"] = false,
+        ["Lovestorm Eel"] = false,
+        ["Lovestorm Eel Supercharged"] = false,
+    },
+
+    meteorList = {
+        ["Amethyst"] = false,
+        ["Ruby"] = false,
+        ["Opal"] = false,
+        ["Lapis Lazuli"] = false,
+        ["Moonstone"] = false,
+    },
+
+    sunkenchestList = {
+        enabled = true,
+        bufferbefore = 1,
+        alertonload = true,
+        autofarm = true,
+        hopafterclaim = true,
+        forcehop = false,
+    },
+
+    version = data_version,
+    versid = data_versid,
+}
+
+local data = {
+    version = data_version,
+    versid = data_versid,
+    updmsg = updmsg,
+    settingchanged = settingchanged,
+    settingmsg = settingmsg,
+    link = link,
+    sunkenchestcoords = sunkenchestcoords,
+    defaultConfig = defaultConfig,
+}
+
+return data
